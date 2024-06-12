fa7979a3eea170c70cd9657fabdbfdcb9430ab0e
bradinator <legendaryphoenix0408@hotmail.com>
2024-06-13 09:01:40 +1000

    Second init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+// main.rs
+
+use sponge_build::convert_to_asm;
+
+// Use the procedural macro to convert Rust code to assembly-like code
+convert_to_asm! {
+    fn main() {
+        let a: i32 = 30;
+    }
 }
