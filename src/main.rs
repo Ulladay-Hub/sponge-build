b16ba1bce0d1d67a1a12e0b18a5ec00d68f8239e
bradinator <legendaryphoenix0408@hotmail.com>
2024-06-17 17:06:18 +1000

    Added code to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-// main.rs
-
-use sponge_build::convert_to_asm;
-
-// Use the procedural macro to convert Rust code to assembly-like code
-convert_to_asm! {
-    fn main() {
-        let a: i32 = 30;
-    }
+fn main() {
+    sponge_build::run();
 }
