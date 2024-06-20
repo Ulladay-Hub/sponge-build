d44505fd65b9a4f8289de3cf9fa550b5e7862463
bradinator <legendaryphoenix0408@hotmail.com>
2024-06-20 15:30:44 +1000

    Idk, this looks goofy :skull:

--- src/parse_tokens.rs
+++ src/parse_tokens.rs
@@ -1,15 +1,36 @@
-use serde::{Serialize, Deserialize};
 use crate::decompose_tokens::Token;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ParsedToken {
-    pub kind: String,
-    pub value: String,
+#[derive(Debug, Clone)]
+pub enum ParsedToken {
+    VariableAssignment { name: String, value: i32 },
 }
 
 pub fn parse(tokens: Vec<Token>) -> Vec<ParsedToken> {
-    tokens.into_iter().map(|token| ParsedToken {
-        kind: token.kind,
-        value: token.value,
-    }).collect()
+    let mut parsed_tokens = Vec::new();
+    let mut iter = tokens.iter().peekable();
+
+    while let Some(token) = iter.next() {
+        if let Token::Keyword(ref kw) = token {
+            if kw == "let" {
+                if let Some(Token::Identifier(name)) = iter.next() {
+                    if let Some(Token::Symbol(':')) = iter.next() {
+                        if let Some(Token::Keyword(typ)) = iter.next() {
+                            if typ == "i32" {
+                                if let Some(Token::Symbol('=')) = iter.next() {
+                                    if let Some(Token::Literal(value)) = iter.next() {
+                                        parsed_tokens.push(ParsedToken::VariableAssignment {
+                                            name: name.clone(),
+                                            value: *value,
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    parsed_tokens
 }
