d834d6751a1fcdca6b9ccedfac23c500b1f250b6
bradinator <legendaryphoenix0408@hotmail.com>
2024-06-23 08:59:04 +1000

    Adjusted errors

--- src/parse_tokens.rs
+++ src/parse_tokens.rs
@@ -2,27 +2,42 @@ use crate::decompose_tokens::Token;
 
 #[derive(Debug, Clone)]
 pub enum ParsedToken {
-    Variable { name: String, var_type: String, value: String },
+    VariableAssignment { name: String, var_type: String, value: String },
     Function { name: String, body: Vec<String> },
 }
 
 
 pub fn parse(tokens: Vec<Token>) -> Vec<ParsedToken> {
     let mut parsed_tokens = Vec::new();
-    let mut iter = tokens.iter().peekable();
 
+    let mut iter = tokens.into_iter();
     while let Some(token) = iter.next() {
-        if let Token::Keyword(ref kw) = token {
-            if kw == "let" {
+        match token {
+            Token::Keyword(keyword) if keyword == "fn" => {
                 if let Some(Token::Identifier(name)) = iter.next() {
-                    if let Some(Token::Symbol(':')) = iter.next() {
-                        if let Some(Token::Keyword(typ)) = iter.next() {
-                            if typ == "i32" {
-                                if let Some(Token::Symbol('=')) = iter.next() {
+                    let mut body = Vec::new();
+                    while let Some(body_token) = iter.next() {
+                        if let Token::Symbol(sym) = &body_token {
+                            if sym == "}" {
+                                break;
+                            }
+                        }
+                        body.push(format!("{:?}", body_token));
+                    }
+                    parsed_tokens.push(ParsedToken::Function { name, body });
+                }
+            }
+            Token::Identifier(name) => {
+                if let Some(Token::Symbol(sym)) = iter.next() {
+                    if sym == ":" {
+                        if let Some(Token::Keyword(var_type)) = iter.next() {
+                            if let Some(Token::Symbol(sym)) = iter.next() {
+                                if sym == "=" {
                                     if let Some(Token::Literal(value)) = iter.next() {
                                         parsed_tokens.push(ParsedToken::VariableAssignment {
-                                            name: name.clone(),
-                                            value: *value,
+                                            name,
+                                            var_type,
+                                            value,
                                         });
                                     }
                                 }
@@ -31,6 +46,7 @@ pub fn parse(tokens: Vec<Token>) -> Vec<ParsedToken> {
                     }
                 }
             }
+            _ => {}
         }
     }
 
